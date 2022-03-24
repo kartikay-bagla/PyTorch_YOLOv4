2db6c337b902d17769b20fd2ef6e0b8a1202455b
kartikay-bagla <kartikaybagla@gmail.com>
2022-03-24 18:51:51 +0530

    updated image, removed redundant function, fixed model device

--- temp.py
+++ temp.py
@@ -5,58 +5,17 @@ from utils.datasets import letterbox
 from models.maskhead import MaskHead
 
 from utils.general import non_max_suppression
+from utils.data_loader import resize_image
 
 from PIL import Image
 
 # from torch.utils.tensorboard import SummaryWriter
 # writer = SummaryWriter('runs/test')
 
-def resize_image(img, bboxes=None, side_length=416):
-    # get height and width of image
-    h, w = img.shape[:2]
-
-    # if it has 2 dimensions only i.e mask, create 2 dim array
-    if len(img.shape) == 2:
-        new_img = np.zeros((side_length, side_length), dtype=img.dtype)
-    # otherwise create a 3 dim array with 3 channels
-    else:
-        new_img = np.zeros((side_length, side_length, 3), dtype=img.dtype)
-
-    # convert to PIL image
-    pil_img = Image.fromarray(img)
-    # image.thumbnail converts the image to 416, x or x, 416 based on the whichever dimension is bigger
-    pil_img.thumbnail((side_length, side_length))
-    # get the new dimensions
-    new_w, new_h = pil_img.size
-
-    # calculate scaling factor
-    scale_factor = side_length / h if h > w else side_length / w
-
-    # since we center the image, need to calculate the offset for the start of the actual image
-    h_start = 0 if h > w else side_length // 2 - new_h // 2
-    w_start = 0 if w > h else side_length // 2 - new_w // 2
-
-    # copy over the resized image into the square image
-    new_img[h_start:h_start+new_h, w_start:w_start+new_w] = np.array(pil_img)
-
-    # update bounding boxes
-    if bboxes:
-        bboxes = [[i*scale_factor for i in bbox] for bbox in bboxes]
-        for bbox in bboxes:
-            bbox[0] = bbox[0] + w_start
-            bbox[1] = bbox[1] + h_start
-
-    # return values
-    if bboxes:
-        return new_img, bboxes
-    else:
-        return new_img
-
-model = MaskHead()
+model = MaskHead(torch.device("cuda")).to("cuda")
 
 img_names = [
-    "test1.jpg",
-    "test2.jpg"
+    "input.jpg"
 ]
 images = []
 for img_name in img_names:
